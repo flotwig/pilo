ba89d575e38a8840434375eb3605357613dca162
Zach Bloomquist <github@chary.us>
2020-08-20 13:39:33 -0400

    chore(ci): add semantic-release

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,10 +1,10 @@
 import { lockKeyboard } from './lock-keyboard'
 import { startVideo } from './video'
 import { startControls } from './controls'
-import { version, repository } from '../../package.json'
+import { version, homepage } from '../../package.json'
 import log from './log'
 
-log.info(`Welcome to Pilo v${version}! Please report any bugs on the <a href="${repository}">GitHub repo</a>.`)
+log.info(`Welcome to Pilo v${version}! Please report any bugs on the <a href="${homepage}">GitHub repo</a>.`)
 
 lockKeyboard()
 startVideo()
